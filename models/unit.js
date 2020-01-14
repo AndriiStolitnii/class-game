45ab671ab93d2663822d3fc90d29d0ce6fe2e8b6
Andrii_Stolitnii <a.s.stoletniy@gmail.com>
2020-01-14 18:30:45 +0200

    start

--- models/unit.js
+++ models/unit.js
@@ -1,14 +1,14 @@
-var Unit = (function () {
+var Unit = (function() {
     function Unit(baseDamage, health, level) {
-        var modifier = (level - 1) * 10;
-        this.damage = baseDamage + modifier;
-        this.maxHealth = this.health = health + modifier;;
-        this.level = level;
+      var modifier = (level - 1) * 10;
+      this.damage = baseDamage + modifier;
+      this.maxHealth = this.health = health + modifier;
+      this.level = level;
     }
-
-    Unit.prototype.attack = function () {
-        console.log('Attack', this);
-    }
-
+  
+    Unit.prototype.attack = function(monster) {
+      return this.health = this.health - monster.damage;
+    };
+  
     return Unit;
-}());
+  })();
