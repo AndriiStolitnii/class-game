5630386c958498877e5bb2948f28be2689352674
Andrii_Stolitnii <a.s.stoletniy@gmail.com>
2020-01-14 17:51:49 +0200

    test

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-var croco = new CrocoTurtle(50);
-var bear = new MonsterBear(2);
+var bg = new BattleGround(document.getElementById('wrapper'), 30);
 
-croco.attack();
-bear.attack();
+bg.fill([Dragon, CrocoTurtle, Monkey, Snake, MonsterBear]);
+bg.addCharacter(new Character());
+
+var fnc = function(e) {
+  switch (e.keyCode) {
+    case 37:
+      bg.moveCharacterLeft();
+      break;
+    case 38:
+      bg.characterJump();
+      break;
+    case 39:
+      bg.moveCharacterRight();
+      break;
+    case 40:
+      bg.characterCrouch();
+      break;
+  }
+};
+
+document.addEventListener('keydown', fnc);
