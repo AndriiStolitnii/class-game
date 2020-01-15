ef3ebb7db82e80347088cf7fc2a6d49c107a759b
Andrii_Stolitnii <a.s.stoletniy@gmail.com>
2020-01-15 14:26:33 +0200

    hw18

--- models/battleground.js
+++ models/battleground.js
@@ -1,14 +1,19 @@
 var BattleGround = (function() {
-  function BattleGround(size, amountOfMonsters) {
+  function BattleGround(element, size, amountOfMonsters) {
+    this.wrapperElement = null;
+    this.cellsElement = null;
+    this.controllsElement = null;
+    this.element = element;
+    this.size = size;
     this.amountOfMonsters = amountOfMonsters || size / 2;
     this.area = new Array(size).fill(1).map(function() {
       return new Grass();
     });
+    this.render();
   }
 
   BattleGround.prototype.fill = function(monstersArr) {
     var self = this;
-    console.log(monstersArr);
     Array(this.amountOfMonsters)
       .fill(1)
       .forEach(function() {
@@ -20,7 +25,37 @@ var BattleGround = (function() {
 
   BattleGround.prototype.addCharacter = function(character) {
     this.area[0] = character;
-  }
+  };
+
+  BattleGround.prototype.render = function() {
+    var el = document.createElement('div');
+    el.classList.add('game__wrapper');
+    var cellsElement = document.createElement('div');
+    cellsElement.classList.add('game__cells');
+    var controllsElement = document.createElement('div');
+    controllsElement.classList.add('game__controls');
+    el.appendChild(cellsElement);
+    el.appendChild(controllsElement);
+    for (let index = 0; index < this.size; index++) {
+      var cell = document.createElement('div');
+      cell.classList.add('game__cell');
+      cellsElement.appendChild(cell);
+    }
+    this.element.appendChild(el);
+  };
+
+  // BattleGround.prototype.update = function() {
+  //   var self = this;
+  //   this.cellsElement.innerHTML = '';
+  //   this.area.forEach(function(el) {
+  //     var cell = document.createElement('div');
+  //     var cellElement = el.render();
+
+  //     cell.classList.add('game__cells__item');
+  //     cell.appendChild(cellElement);
+  //     self.cellsElement.appendChild(cell);
+  //   });
+  // };
 
   return BattleGround;
 })();
